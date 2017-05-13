392c832ff2ac5f4b50156e7f77f471f85f85cb9c
Subigya Nepal <subigya.nepal@deerwalk.edu.np>
2017-05-13 22:13:23 +0545

    python

--- compilerapi.py
+++ compilerapi.py
@@ -27,9 +27,32 @@ class RunC(Resource):
       response.append({'success':'true','output':q})
       return jsonify(result = response)
       
+
+class RunPython(Resource):
+   def post(self):
+      parser = reqparse.RequestParser()
+      parser.add_argument('code', type=str, required=True, location='json')
+      args = parser.parse_args(strict=True)
+      code=urllib.unquote(args['code']).decode('utf8') 
+      print code
+      response = []
+      with open('test.py', 'w') as file:
+         file.write(code)
+      p = subprocess.Popen(['python', 'test.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      out, err = p.communicate()
+      if (err):
+          print "Error"
+          response.append({'success':'false','error':err})
+          return jsonify(result = response)
+      print "Success"
+      #q = subprocess.check_output(['./test'])
+      response.append({'success':'true','output':out})
+      return jsonify(result = response)
       
+           
         
 api.add_resource(RunC, '/C/')
+api.add_resource(RunPython, '/python/')
 
 if __name__ == '__main__':
     app.run(threaded=True)
